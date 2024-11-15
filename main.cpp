62da5817aa30c740220029a8cb84197726624d8b
Daniil <daniilk@gmail.com>
2024-11-15 13:53:23 +0500

    Написание каркаса функций

--- main.cpp
+++ main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
+void read_file(const string& file, vector<string>& lines)
+{
+
+}
+
+void print_lines(const vector<string>& lines)
+{
+
+}
+
+void write_file(const string& file,const vector<string>& lines)
+{
+
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    vector<string> lines;
+    string input_file = "input.txt";
+    string output_file = "output.txt";
+
+    read_file(input_file, lines);
+    print_lines(lines);
+    write_file(output_file, lines);
     return 0;
 }
