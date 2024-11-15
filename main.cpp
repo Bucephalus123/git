e345b8d95105ff6be78c320993eac1f39e373036
Yarik <yaroslav.golovanov.05@mail.ru>
2024-11-15 14:08:25 +0500

    Проверка, что все работает корректно

--- main.cpp
+++ main.cpp
@@ -13,11 +13,19 @@ void read_file(const string& file, vector<string>& lines)
 void print_lines(const vector<string>& lines)
 {
 
+    for(auto &x: lines)
+    {
+        cout << x << endl;
+    }
 }
 
 void write_file(const string& file,const vector<string>& lines)
 {
-
+    ofstream out("output.txt");
+    for(auto &x: lines)
+    {
+        out << x << endl;
+    }
 }
 
 int main()
@@ -27,6 +35,7 @@ int main()
     string output_file = "output.txt";
 
     read_file(input_file, lines);
+    lines = {10,"a"};
     print_lines(lines);
     write_file(output_file, lines);
     return 0;
